2966a3d3548bd30cfd765174e4b31ad19df8ec63
Skyler <smithskylera@gmail.com>
2018-04-13 17:33:00 -0600

    tried using static dist

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 // Knex Setup
 const env = process.env.NODE_ENV || 'development';
